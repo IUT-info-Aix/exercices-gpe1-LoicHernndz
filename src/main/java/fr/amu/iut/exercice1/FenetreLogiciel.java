6bba9329506ecd4ae1f1b690cc113ad202db1d9b
Loïc Hernandez <loic.hernandez@etu.univ-amu.fr>
2025-04-22 16:20:24 +0200

    Exercice 1 : - Ajout de la barre inferieur avec un separateur visuel - Passage a un BorderPane pour l'organisation de la scene

--- src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
+++ src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
@@ -1,10 +1,15 @@
 package fr.amu.iut.exercice1;
 
 import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,7 +20,7 @@ public class FenetreLogiciel extends Application {
     public void start(Stage primaryStage) {
 
         // Conteneur Principal
-        VBox vbox = new VBox();
+        BorderPane root = new BorderPane();
 
         // Menu supérieur
         Menu menu1 = new Menu("File");
@@ -34,20 +39,23 @@ public class FenetreLogiciel extends Application {
         Menu menu3 = new Menu("Help");
 
         MenuBar menuBar = new MenuBar(menu1, menu2, menu3);
+        root.setTop(menuBar);
+        // Bandeau inférieur de prompt
 
-        // Ajout des contrôleurs au conteneur principal
-        vbox.getChildren().addAll(
-            menuBar
-        );
+        Label prompts = new Label ("Ceci est un label de bas de page");
+        Separator bottomSeperator = new Separator(Orientation.HORIZONTAL);
+        VBox bottomBorder = new VBox(bottomSeperator, prompts);
+        bottomBorder.setAlignment(Pos.CENTER);
+        root.setBottom(bottomBorder);
 
         // Ajout du conteneur à la scene
-        Scene scene = new Scene(vbox);
+        Scene scene = new Scene(root);
 
         // Ajout de la scene à la fenêtre et changement de ses paramètres (dimensions et titre)
         primaryStage.setScene(scene);
         primaryStage.setWidth(800);
         primaryStage.setHeight(600);
-        primaryStage.setTitle("Exercice 1");
+        primaryStage.setTitle("Premier exemple manipulant les conteneurs");
 
         // Affichage de la fenêtre
         primaryStage.show();
