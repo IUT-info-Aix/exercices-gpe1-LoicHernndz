60e87f732bba9ee12bb35578f2397d5a6a4adffe
Loïc Hernandez <loic.hernandez@etu.univ-amu.fr>
2025-04-22 17:17:49 +0200

    FIN Exercice 1 - Ajout du contenu central

--- src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
+++ src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
@@ -4,12 +4,10 @@ import javafx.application.Application;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.Separator;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -40,12 +38,67 @@ public class FenetreLogiciel extends Application {
 
         MenuBar menuBar = new MenuBar(menu1, menu2, menu3);
         root.setTop(menuBar);
-        // Bandeau inférieur de prompt
 
+        // Sidebar de boutons
+        VBox sideBar = new VBox();
+
+        Label labelSideBar = new Label("Boutons :");
+        Button btn1SideBar = new Button("Bouton 1");
+        Button btn2SideBar = new Button("Bouton 2");
+        Button btn3SideBar = new Button("Bouton 3");
+        sideBar.getChildren().addAll(labelSideBar,btn1SideBar,btn2SideBar,btn3SideBar);
+        sideBar.setAlignment(Pos.CENTER);
+        sideBar.setSpacing(10);
+
+        Separator leftSeperator = new Separator(Orientation.VERTICAL);
+        HBox leftBorder = new HBox(sideBar, leftSeperator);
+
+        root.setLeft(leftBorder);
+
+        // Formulaire central
+        VBox centerBorder = new VBox();
+
+        GridPane formulaire = new GridPane();
+        formulaire.setAlignment(Pos.CENTER);
+        formulaire.setHgap(10);
+        formulaire.setVgap(10);
+
+        Label labelName = new Label("Name:");
+        GridPane.setConstraints(labelName,0,0);
+        TextField txtFieldName = new TextField();
+        GridPane.setConstraints(txtFieldName,1,0);
+
+        Label labelEmail = new Label("Email:");
+        GridPane.setConstraints(labelEmail,0,1);
+        TextField txtFieldEmail = new TextField();
+        GridPane.setConstraints(txtFieldEmail,1,1);
+
+        Label labelPassword = new Label("Password:");
+        GridPane.setConstraints(labelPassword,0,2);
+        TextField txtFieldPassword = new TextField();
+        GridPane.setConstraints(txtFieldPassword,1,2);
+
+        HBox centerButtons = new HBox();
+        Button btnSubmit = new Button("Submit");
+        Button btnCancel = new Button("Cancel");
+        centerButtons.setAlignment(Pos.CENTER);
+        centerButtons.setSpacing(10);
+
+        formulaire.getChildren().addAll(labelName,txtFieldName,labelEmail,txtFieldEmail,labelPassword,txtFieldPassword);
+        centerButtons.getChildren().addAll(btnSubmit,btnCancel);
+        centerBorder.getChildren().addAll(formulaire,centerButtons);
+        centerBorder.setAlignment(Pos.CENTER);
+        centerBorder.setSpacing(10);
+
+        root.setCenter(centerBorder);
+
+        // Bandeau inférieur de prompt
         Label prompts = new Label ("Ceci est un label de bas de page");
+
         Separator bottomSeperator = new Separator(Orientation.HORIZONTAL);
         VBox bottomBorder = new VBox(bottomSeperator, prompts);
         bottomBorder.setAlignment(Pos.CENTER);
+
         root.setBottom(bottomBorder);
 
         // Ajout du conteneur à la scene
